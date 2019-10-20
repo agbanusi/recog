cc7ea8b4cff3bb02824ee3c1061b76030092496c
agbanusi <53221092+agbanusi@users.noreply.github.com>
2019-10-20 11:49:28 +0100

    Update rest.py

--- rest.py
+++ rest.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import facial_recognition
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS, cross_origin 
 import facial_recognition
@@ -7,11 +8,28 @@ CORS(app)
 api = Api(app)
 
 #port = int(os.getenv('PORT', 8080)) 
-class check(Resource):
-    def get(self):
-        return {'data': facial_recognition.check()}
+@app.route('/',methods = ['GET','POST'])
+def get(self):
+    if request.method == 'POST':
+        data=request.form.get('text')
+        facial_recognition()
+        if data == 'check':
+            result = facial_recognition.check()
+            print(result)
+        if data == 'add embeddings':
+            dat = request.form.get('which addition?')
+            if dat == 'visitor':
+                facial_recognition.add_visitor_embedding()
+                print('done!')
+            elif dat == 'member':
+                facial_recognition.add_member_embedding()
+                print('done!')
+            else:
+                None
+                print('Not Allowed!')
+        
+    return {'data': result}
 
-api.add_resource(check, '/facialrecognition/')
     
 if __name__ == '__main__': 
     app.run() # deploy with debug=False
